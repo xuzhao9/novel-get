04dd6073115a48e26ad9eafff62cc0b56b0afccd
Xu Zhao <i@xuzhao.net>
2017-07-03 15:26:35 -0400

    Crawled everyting from xunsee.com

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./novel-get http://www.xunsee.com/article/8d66b983-7831-4748-b86b-a580ef20e633/index.shtml
+./novel-get http://www.xunsee.com/article/8d66b983-7831-4748-b86b-a580ef20e633/index.shtml out.txt
--- src/novel_get/__main__.py
+++ src/novel_get/__main__.py
@@ -8,13 +8,13 @@ _help = """Usage: {} [OPTION] ... [URL] ...
 
 def crawl_pages(pages):
     content_array = []
-    for p in pages:
+    for idx, p in enumerate(pages):
+        print("Crawling " + idx + "/" + len(pages))
         c = urllib.request.urlopen(p).read().decode('gbk')
         content_array.append(c)
     return content_array
 
-def parse_xunsee(url):
-    print("parsing xunsee!")
+def parse_xunsee(url, ofile):
     split = url.rsplit('/', 1) # cut into directory/page
     content = urllib.request.urlopen(url).read().decode('gbk')
     soup = BeautifulSoup(content, 'lxml').find_all(id="content_1")
@@ -22,15 +22,35 @@ def parse_xunsee(url):
     pages = list(map(lambda x: x.find_all('a')[0].get('href'), spants))
     pages = list(map(lambda x: split[0] + '/' + x, pages))
     # crawl only first five pages
-    pages = pages[:2]
+    # pages = pages[:2]
     content_array = crawl_pages(pages)
-    for c in content_array:
-        print(c)
+    article_array = []
+    for idx, c in enumerate(content_array):
+        csoup = BeautifulSoup(c, 'lxml').find(id="content_1")
+        article = csoup.get_text().strip().replace("　　", "　")\
+                                          .replace("上一页  目录  下一页", "")\
+                                          .replace("目录  下一页", "")
+        article_array.append(article)
+    out = '\n'.join(article_array)
+    dumper(out, ofile)
+
+# convert novel to txt file
+def dumper(novel, ofile):
+    if ofile.endswith(".txt"):
+        print("Dumping txt...")
+        out = open(ofile, 'w')
+        out.write(novel)
+        out.close()
+    elif ofile.endswith(".pdf"):
+        print("Dumping pdf...")
+    else:
+        print("Only txt and pdf formats are supported!")
 
 def main(**kwargs):
     url = kwargs['url']
+    out = kwargs['out']
     if "xunsee" in url.lower():
-        parse_xunsee(url)
+        parse_xunsee(url, out)
     
 if __name__ == '__main__':
     main()
